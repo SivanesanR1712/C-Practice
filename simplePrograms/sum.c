d4eb23a6c26cf65e06760e16256e1dcc02b6e1fb
SivanesanR1712 <sivanesanr17@outlook.com>
2026-02-12 20:56:41 +0530

    Day 2: C program basics

---
+++ simplePrograms/sum.c
@@ -0,0 +1,13 @@
+/* Author Sivanesan R
+Date : 2/12/2026
+File: sum.c - to print sum off two number
+*/
+
+#include<stdio.h>
+int main(){
+    int a, b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int sum= a+b;
+    printf("sum=%d",sum);
+}
